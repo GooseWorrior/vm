a33ac85b8d9825d42ff48a3f4b187902e17d032f
Han <hasd@gm>
2019-11-18 23:14:24 -0500

    Moved files around to make more sense

--- src/editorComponent/vm.h
+++ src/editorComponent/vm.h
@@ -1,14 +1,21 @@
 #ifndef VM_H
 #define VM_H
 
-#include "../model/Model.h"
+#include <string>
+
+#include "../Model.h"
 
 #include "Commandline.h"
 #include "Cursor.h"
 #include "EditorComponent.h"
 #include "StatusLine.h"
 
+using std::string;
+
 namespace CS246E {
-class VM : public Model {};
+class VM : public Model {
+ public:
+  VM(string filename) {}
+};
 }  // namespace CS246E
 #endif
--- src/vmmain.d
+++ src/vmmain.d
@@ -1,2 +1,6 @@
-src/vmmain.o: src/vmmain.cc src/model/Model.h \
- src/model/../controller/Controller.h src/model/../view/view.h
+src/vmmain.o: src/vmmain.cc src/EditorComponent/VM.h \
+ src/EditorComponent/../Model.h \
+ src/EditorComponent/../controller/Controller.h \
+ src/EditorComponent/../view/View.h src/EditorComponent/Commandline.h \
+ src/EditorComponent/EditorComponent.h src/EditorComponent/Cursor.h \
+ src/EditorComponent/StatusLine.h
