c54833bf088a892a38aa5f1118e77aaf99e0201e
Han <hasd@gm>
2019-11-24 17:35:56 -0500

    Merged

--- src/view/PlainView.cc
+++ src/view/PlainView.cc
@@ -6,20 +6,20 @@ PlainView::PlainView(VM* vm) : View{} { this->vm = vm; }
 void PlainView::display(pair<int, int> prevPointer, int input,
                         pair<int, int> prevCursor, int prevChar, int prevSize,
                         bool edit) {
-  if (vm->updateWindowSize() ||
-      (prevPointer.first != vm->WindowPointer.first &&
-       prevPointer.second != vm->WindowPointer.second &&
-       vm->WindowPointer.second - vm->WindowPointer.first + 1 <
-           vm->text.size())) {
-    vm->printTextAll();
-  } else if (vm->text.size() != prevSize) {
-    vm->printTextAfterward(input, prevCursor);
-  } else if (edit &&
-             vm->vcursor.getCol() != vm->text[vm->vcursor.getRow()].size()) {
-    vm->printTextLine(input, prevCursor, prevChar);
-  } else if (edit) {
-    vm->printTextChar(input, prevChar);
-  }
+  // if (vm->updateWindowSize() ||
+  //     (prevPointer.first != vm->WindowPointer.first &&
+  //      prevPointer.second != vm->WindowPointer.second &&
+  //      vm->WindowPointer.second - vm->WindowPointer.first + 1 <
+  //          vm->text.size())) {
+  //   vm->printTextAll();
+  // } else if (vm->text.size() != prevSize) {
+  //   vm->printTextAfterward(input, prevCursor);
+  // } else if (edit &&
+  //            vm->vcursor.getCol() != vm->text[vm->vcursor.getRow()].size()) {
+  //   vm->printTextLine(input, prevCursor, prevChar);
+  // } else if (edit) {
+  //   vm->printTextChar(input, prevChar);
+  // }
 }
 void PlainView::update() {}
 PlainView::~PlainView() {}
