d29e835cfd869e8a7ef0d12de71209e28c04c016
Han <hasd@gm>
2019-12-01 21:40:06 -0500

    Auto stash before merge of "feature/han" and "origin/feature/han"

--- src/vmmain.d
+++ src/vmmain.d
@@ -1,9 +1,9 @@
-src/vmmain.o: src/vmmain.cc src/EditorComponent/VM.h \
- src/EditorComponent/../Model.h src/EditorComponent/../view/PlainView.h \
- src/EditorComponent/../view/../editorComponent/VM.h \
- src/EditorComponent/../view/View.h \
- src/EditorComponent/../view/SyntaxView.h \
- src/EditorComponent/Commandline.h src/EditorComponent/EditorComponent.h \
- src/EditorComponent/StatusLine.h src/EditorComponent/Cursor.h \
- src/controller/Keyboard.h src/controller/Controller.h \
+src/vmmain.o: src/vmmain.cc src/controller/Keyboard.h \
+ src/controller/Controller.h src/editorComponent/VM.h \
+ src/editorComponent/../Model.h src/editorComponent/../view/PlainView.h \
+ src/editorComponent/../view/../editorComponent/VM.h \
+ src/editorComponent/../view/View.h \
+ src/editorComponent/../view/SyntaxView.h \
+ src/editorComponent/CommandLine.h src/editorComponent/EditorComponent.h \
+ src/editorComponent/Cursor.h src/editorComponent/StatusLine.h \
  src/view/PlainView.h
