1a54186fe32fdba2ea1ed48eed5c2d82f64593b4
Han <hasd@gm>
2019-12-02 22:04:40 -0500

    Minor code cleanup

--- src/view/PlainView.cc
+++ src/view/PlainView.cc
@@ -48,9 +48,6 @@ void PlainView::printPlaceholder() {
 
 void PlainView::printTextAll() {
   clear();
-  /*for (auto i : text) {
-    printw("%s\n", i.c_str());
-  }*/
   for (size_t i = vm->WindowPointer.first; i <= vm->WindowPointer.second; ++i) {
     printw("%s\n", vm->text[i].c_str());
   }
--- src/view/SyntaxView.h
+++ src/view/SyntaxView.h
@@ -1,52 +1,57 @@
 #ifndef SYNTAX_VIEW_H
 #define SYNTAX_VIEW_H
 
+#include <map>
 #include <memory>
-#include <vector>
-#include <string>
-#include <map> 
 #include <regex>
+#include <string>
+#include <vector>
 
 #include "../editorComponent/VM.h"
 #include "View.h"
 
 using std::map;
-using std::string;
-using std::vector;
-using std::unique_ptr;
 using std::regex;
-using std::smatch;
 using std::regex_search;
+using std::smatch;
+using std::string;
+using std::unique_ptr;
+using std::vector;
 
 namespace CS246E {
 class VM;
 class SyntaxView : public View {
-  VM * vm;
+  VM* vm;
   vector<pair<int, pair<regex, int>>> renderLibrary;
- public:
-  SyntaxView(VM * vm);
-  void update() override;
-  void display(pair<int, int> prevPointer, int input, pair<int, int> prevCursor,
-               pair<int, int> prevWindowSize, int prevChar, int prevSize, bool edit) override;
-  ~SyntaxView();
-  
-  // transport functions
-  void printTextAll();  // temporary
   void printTextAfterward(int input, pair<int, int> prevCursor);
   void printTextLine(int input, pair<int, int> prevCursor,
                      int prevChar);  // temporary
   void printTextChar(int input, int prevChar);
-  void printPlaceholder(); 
+  void loadLibrary();  // move to View
+  void render(
+      vector<pair<int, int>>& singleComment,
+      vector<pair<bool, pair<int, int>>>& multiComment);  // move to View
+  pair<int, int> CloseBracket(char openBracket, char closeBracket,
+                              pair<int, int> local);
+  pair<int, int> OpenBracket(char openBracket, char closeBracket,
+                             pair<int, int> local);
 
-  void initialize();
-  void loadLibrary(); // move to View
-  void render(vector<pair<int, int>> & singleComment,  vector<pair<bool, pair<int, int>>> & multiComment); // move to View
-  pair<int, int> CloseBracket(char openBracket, char closeBracket, pair<int, int> local);
-  pair<int, int> OpenBracket(char openBracket, char closeBracket, pair<int, int> local);
   void highlightBracket();
   bool inRange(int row);
   pair<int, int> transferLoc(int row, int col);
 
+ public:
+  SyntaxView(VM* vm);
+  void update() override;
+  void display(pair<int, int> prevPointer, int input, pair<int, int> prevCursor,
+               pair<int, int> prevWindowSize, int prevChar, int prevSize,
+               bool edit) override;
+  ~SyntaxView();
+
+  // transport functions
+  void printTextAll();  // temporary
+  void printPlaceholder();
+  void initialize();
 };
 }  // namespace CS246E
 #endif
