217e5deff263d659896ea6d3884ab85929fdab4f
Han <hasd@gm>
2019-12-03 21:45:58 -0500

    Auto stash before merge of "feature/han" and "origin/feature/han"

--- src/vmmain.d
+++
@@ -1,7 +0,0 @@
-src/vmmain.o: src/vmmain.cc src/controller/Keyboard.h \
- src/controller/Controller.h src/editorComponent/VM.h \
- src/editorComponent/../Model.h src/editorComponent/../view/PlainView.h \
- src/editorComponent/../view/View.h \
- src/editorComponent/../view/SyntaxView.h \
- src/editorComponent/EditorComponent.h src/editorComponent/Cursor.h \
- src/editorComponent/StatusLine.h src/view/PlainView.h
